55d4264a2b47a78b986f955cc96db46cb44cbb46
g.yakimenko <nneeleps@gmail.com>
2022-09-21 13:34:04 +0300

    add readme

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import getRandomElements from "./getRandomElements";
+import Table from "./table";
 import getArrayOfUniqueValues from "./utils/getArrayOfUniqueValues";
 
 /*
-    Здесь вы можете как угодно эксперементировать с написанной вами ф-ей;
+    Здесь вы можете как угодно эксперементировать с написанным вами кодом;
 
     ARRAY_OF_UNIQUE_VALUES - массив уникальных значений из N элементов,
     возможно, будет полезно :)
 
 */
 
+//********************************************************************* 
+
 const N = 100;
 const ARRAY_OF_UNIQUE_VALUES = getArrayOfUniqueValues(N);
 
 const result = getRandomElements(ARRAY_OF_UNIQUE_VALUES, 5);
 console.log(result);
+
+
+//********************************************************************* 
+
+const table = new Table();
+table.print();
